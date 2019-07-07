f8af929124ad49e852e99db3df9bff09d2d7b6e0
Chandler DeLoach <deloachcd@g.cofc.edu>
2019-07-07 16:30:58 -0400

    pushing 'remove_login' to SCM before converting to generalized form

--- lazy_modules/remove_login.py
+++ lazy_modules/remove_login.py
@@ -11,6 +11,20 @@ from lazy_modules.share.login_table_helpers import (
 from lazy_modules.share.user_input_helpers import user_input_answer
 
 
+def delete_single_entry(table, index):
+    del table[index]
+
+
+def delete_multiple_entries(table, login_query):
+    modified_table = list(
+        filter(
+            lambda row: not all([query in row for query in login_query]),
+            table
+        )
+    )
+    return modified_table
+
+
 def main(*args):
     parser = argparse.ArgumentParser(
         description='Remove an entry from the login table',
@@ -22,12 +36,15 @@ def main(*args):
     table_rows = read_login_table()
     query_rows = query_table(args.login_query, table_rows)
 
-    print(render_table(query_rows, number_rows=True))
-    user_deletion_answer = input(
-        "Which entry would you like to remove? [1-{}, or '*' for all] ".format(
-            len(query_rows)
+    if len(query_rows) > 1:
+        print(render_table(query_rows, number_rows=True))
+        user_deletion_answer = input(
+            "Which entry would you like to remove? [1-{}, or '*' for all] ".format(
+                len(query_rows)
+            )
         )
-    )
+    else:
+        user_deletion_answer = '1'
 
     if user_deletion_answer == '*':
         # delete all rows matched by the query
@@ -56,3 +73,4 @@ def main(*args):
         exit()
 
     write_login_table(table_rows)
+    print("Done.")
